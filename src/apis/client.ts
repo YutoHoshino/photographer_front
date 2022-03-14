4a31796bfccd1223cbf522b30ca1158ad100c349
YutoHoshino <yutohoshino@Mac.local>
2022-03-14 15:06:32 +0900

    本番と開発でapi取得url変更

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -1,6 +1,14 @@
 import axios from "axios"
 
+var url;
+if (process.env.NODE_ENV === 'development') {
+  url = 'http://localhost:3001/api/v1';
+
+} else if (process.env.NODE_ENV === 'production') {
+  url = 'https://photographar-rails-backend.herokuapp.com/api/v1';
+}
+
 export const client = axios.create({
-  baseURL: "http://localhost:3001/api/v1",
+  baseURL: url,
   withCredentials: true
 })
